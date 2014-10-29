3fb857ccddd705d5e4e2f631e50eb334d1f10396
Richard Japenga <treurmars@gmail.com>
2014-10-29 15:20:30 +0100

    updated with ruby check and sass check

--- autobot.sh
+++ autobot.sh
@@ -3,13 +3,26 @@
 while test $# -gt 0; do
   case "$1" in
     --init)
-      echo "first install all the npm stuff, this will create a node_modules folder"
+      # before we can install things we need to check if there are the following things available
+      # 1. ruby
+
+      if ! hash ruby 2> /dev/null; then
+        echo "ruby is not installed, please install ruby and run this script again"
+        break
+      fi
+
+      # 2. sass
+      if ! hash sass 2> /dev/null; then
+        echo "Sass is not installed, adding gem"
+        gem install sass
+        echo "Installed Sass"
+      fi
+
+      echo "install the npm stuff, this will create a node_modules folder"
       echo "let's check if there is a package.json"
-      if [ -f 'package.json' ];
-      then
+      if [ -f 'package.json' ]; then
         echo "found the package.json, let's see if there is anything installed"
-        if [ -d 'node_modules' ];
-        then
+        if [ -d 'node_modules' ]; then
           echo "node_modules folder exists, running update"
           npm update
         else
@@ -19,8 +32,15 @@ while test $# -gt 0; do
       else
         echo "couldn't find package.json, please add your package.json file."
       fi
+
       # done with the npm part! Now onto sass
-      
+      # make sass directory
+      # set source directory?
+      echo "creating sass directory"
+      mkdir "sass"
+      echo "creating output directory"
+      mkdir "public"
+      echo "creating"
       break
       ;;
 
@@ -30,6 +50,11 @@ while test $# -gt 0; do
       break
       ;;
 
+    --start)
+    # make sure everything is fine and dandy? and then run?
+      grunt watch
+      break
+      ;;
+    # grunt watch task
   esac
 done
-
